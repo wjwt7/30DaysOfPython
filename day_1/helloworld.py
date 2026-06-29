a731a0ec1c79bc0fd30d9ab9edc88b08ba7f41fe
wjwt7 <wilsontruong7@outlook.com>
2026-06-29 09:31:20 -0400

    Level2

---
+++ day_1/helloworld.py
@@ -0,0 +1,35 @@
+print('Arithmetic Operations')
+print('Addition')
+print(3 + 4)
+print('Subtraction')
+print(3 - 4)
+print('Multiplication')
+print(3 * 4)
+print('Modulus')
+print(3 % 4)
+print('Division')
+print(3 / 4)
+print('Exponential')
+print(3 ** 4)
+print('Floor Division')
+print(3 // 4)
+
+print('Strings')
+print('First name')
+print('Wilson')
+print('Last name')
+print('Truong')
+print('Country')
+print('Canada')
+print('I am enjoying 30 days of python')
+
+print('Checking data types')
+print(type(10))          # Int
+print(type(9.8))         # Float
+print(type(3.14))        # Float
+print(type(4 + 4j))      # Complex number
+print(type(['Asabeneh', 'Python', 'Finland']))   # List
+print(type('Wilson'))  # String
+print(type('Truong'))  # String
+print(type('Canada'))  # String
+
