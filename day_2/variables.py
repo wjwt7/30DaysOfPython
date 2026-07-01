399c4d54e65326f2f6edcf16d19908cab2f4ff49
wjwt7 <wilsontruong7@outlook.com>
2026-06-30 22:57:25 -0400

    Level1

---
+++ day_2/variables.py
@@ -0,0 +1,12 @@
+# Day 2: 30 Days of python programming
+first_name = 'Wilson'  # string
+last_name = 'Truong'   # string
+full_name = 'Wilson Truong'
+country = 'Canada'
+city = 'Montreal'
+age = 20
+year = 2026
+is_married = False
+is_true = True
+is_light_on = True
+height, weight = 1.75, 68.0
