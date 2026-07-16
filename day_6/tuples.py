19c2c47cab8c17f9496883d82732244e16f834e7
wjwt7 <wilsontruong7@outlook.com>
2026-07-16 18:23:30 -0400

    Level 1 complete

---
+++ day_6/tuples.py
@@ -0,0 +1,16 @@
+#Exercise 1: Level 1
+#1
+empty_tuple = ()
+#2
+brothers = ('Bro', 'bruh', 'brother')
+sisters = ('sister', 'shi')
+#3
+siblings = brothers + sisters
+print(siblings)
+#4
+print(len(siblings))
+#5
+family_members = siblings + ('Father', 'Mother')
+print(family_members)
+
+
