0dcbf04891681714055d2a763c283a0106cb1982
wjwt7 <wilsontruong7@outlook.com>
2026-07-16 18:32:57 -0400

    Completed level 2 tuple

--- day_6/tuples.py
+++ day_6/tuples.py
@@ -13,4 +13,30 @@ print(len(siblings))
 family_members = siblings + ('Father', 'Mother')
 print(family_members)
 
-
+#Exercise: Level 2
+#1
+bro1, bro2, bro3, sis1, sis2, father, mother = family_members
+print(bro1, bro2, bro3, sis1, sis2, father, mother)
+#2
+fruits = ('banana', 'orange', 'mango', 'lemon')
+vegetables = ('Tomato', 'Potato', 'Cabbage', 'Onion', 'Carrot')
+animal_products = ('Milk', 'Meat', 'Butter', 'Yogurt')
+food_stuff_tp = fruits + vegetables + animal_products
+print(food_stuff_tp)
+#3
+food_stuff_lt = list(food_stuff_tp)
+print(food_stuff_lt)
+#4
+middle_item = food_stuff_lt[len(food_stuff_lt) // 2]
+print(middle_item)
+#5
+first_three = food_stuff_lt[:3]
+print(first_three)
+last_three = food_stuff_lt[-3:]
+print(last_three)
+#6
+del food_stuff_tp
+#7
+nordic_countries = ('Denmark', 'Finland', 'Iceland', 'Norway', 'Sweden')
+print('Estonia' in nordic_countries)
+print('Iceland' in nordic_countries)
